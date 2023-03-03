8833321f4db0fcfd1525419f94898db07c9a9565
pooh <cktjdgus45@naver.com>
2023-03-03 10:20:29 +0900

    js-String

---
+++ BuiltInObject/String/String.js
@@ -0,0 +1,33 @@
+//String 생성자 함수 객체.
+
+String.prototype.charAt
+String.prototype.charCodeAt
+String.prototype.codePointAt
+String.prototype.concat
+// String.prototype.endsWith
+// String.prototype.includes
+// String.prototype.indexOf
+String.prototype.lastIndexOf
+// String.prototype.search
+// String.prototype.length
+// String.prototype.substring(1,4)  1~4까지의 문자 반환.
+// String.prototype.slice
+String.prototype.localeCompare
+String.prototype.match
+// String.prototype.toUpperCase
+// String.prototype.toLowerCase
+// String.prototype.trim
+// String.prototype.repeat
+// String.prototype.replace
+// String.prototype.split
+String.prototype.valueOf
+// String.prototype.startsWith
+String.call
+String.bind
+String.apply
+String.arguments
+String.caller
+String.length
+String.name
+
+
